4da32708fa798ef37b39b258234a5beb53317434
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-13 17:20:29 +0300

    feat: Add scroll reveal effect to header elements
    - Implemented scroll reveal animation for header image, header title, description, and button
    - Improved user experience with engaging animations upon scrolling

--- 5-restaurant-landing-page/dist/js/main.js
+++ 5-restaurant-landing-page/dist/js/main.js
@@ -13,3 +13,29 @@ navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 500,
+});
